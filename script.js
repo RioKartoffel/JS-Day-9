266810d933d51f0f45edc7703cb6652d9d01475c
Rio-Kartoffel <riokartoffel09@gmail.com>
2023-01-26 08:23:10 +0800

    edit

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ['game over', 'game notification', 'morse code', 'tadaa', 'windows error']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
